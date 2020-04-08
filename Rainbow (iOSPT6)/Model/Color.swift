c1e79b7bab8087fdab1228ecfc8c22fb3d240ed1
Aaron Peterson <aarondp2011@gmail.com>
2020-04-07 21:34:52 -0500

    Setup color model

---
+++ Rainbow (iOSPT6)/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow (iOSPT6)
+//
+//  Created by Aaron Peterson on 4/7/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
