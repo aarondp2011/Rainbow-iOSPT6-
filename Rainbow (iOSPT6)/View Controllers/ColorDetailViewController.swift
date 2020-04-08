6506ed2e88eb3ff09f64ea709b56a46013118903
Aaron Peterson <aarondp2011@gmail.com>
2020-04-07 21:25:53 -0500

    Organized folders and created VC files

---
+++ Rainbow (iOSPT6)/View Controllers/ColorDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorDetailViewController.swift
+//  Rainbow (iOSPT6)
+//
+//  Created by Aaron Peterson on 4/7/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
